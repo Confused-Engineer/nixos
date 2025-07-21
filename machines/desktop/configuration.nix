2ca908dc453f43b653dbd88f1e95fa3ec8710bef
Confused-Engineer <dpierce33100@gmail.com>
2025-07-20 20:14:34 -0400

    further improved compatibility

--- machines/desktop/configuration.nix
+++ machines/desktop/configuration.nix
@@ -58,20 +58,30 @@
     
   };
 
-  hardware.xpadneo.enable = true;
-  hardware.bluetooth.settings = {
-  General = {
-    Privacy = "device";
-    JustWorksRepairing = "always";
-    Class = "0x000100";
-    FastConnectable = true;
+hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings.General = {
+      experimental = true; # show battery
+
+      # https://www.reddit.com/r/NixOS/comments/1ch5d2p/comment/lkbabax/
+      # for pairing bluetooth controller
+      Privacy = "device";
+      JustWorksRepairing = "always";
+      Class = "0x000100";
+      FastConnectable = true;
     };
   };
+  services.blueman.enable = true;
+
+  hardware.xpadneo.enable = true; # Enable the xpadneo driver for Xbox One wireless controllers
+
   boot = {
     extraModulePackages = with config.boot.kernelPackages; [ xpadneo ];
     extraModprobeConfig = ''
       options bluetooth disable_ertm=Y
     '';
+    # connect xbox controller
   };
 
   # Flakes
