79427d459d825726d7e8b12b1d2a30f5c7197abb
Confused-Engineer <dpierce33100@gmail.com>
2025-08-15 20:31:34 -0400

    mullvad on laptop for testing

--- machines/laptop/configuration.nix
+++ machines/laptop/configuration.nix
@@ -63,6 +63,19 @@
     
   };
 
+  services.mullvad-vpn.enable = true;
+  services.mullvad-vpn.package = pkgs.mullvad-vpn;
+
+
+ # networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+ # services.resolved = {
+ #   enable = true;
+ #   dnssec = "true";
+ #   domains = [ "~." ];
+ #   fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+ #   dnsovertls = "true";
+ # };
+
   nix.settings.experimental-features = ["nix-command" "flakes"];
   boot.kernelPackages = pkgs.unstable.linuxPackages;
 
