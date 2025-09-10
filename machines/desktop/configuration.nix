236950891b4171e645badd5de5b7318e9696e972
Confused-Engineer <dpierce33100@gmail.com>
2025-09-09 20:05:31 -0400

    add razer apps and update flake

--- machines/desktop/configuration.nix
+++ machines/desktop/configuration.nix
@@ -193,12 +193,15 @@
     python312
     python312Packages.venvShellHook
     prismlauncher
-   # polychromatic
-   # openrazer-daemon
+    polychromatic
+    openrazer-daemon
     brave
     procps
     gparted
   ];
+
+  hardware.openrazer.enable = true;
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
