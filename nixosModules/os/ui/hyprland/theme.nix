99461bce2e699cc72fbdac549d5f3be1bae10f08
Confused-Engineer <dpierce33100@gmail.com>
2025-10-18 08:13:16 -0400

    dconf for dark mode

--- nixosModules/os/ui/hyprland/theme.nix
+++ nixosModules/os/ui/hyprland/theme.nix
@@ -1,21 +1,11 @@
 { pkgs, ... }: {
-
-  
-
   dconf = {
     enable = true;
-    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-  };
-
-  gtk = {
-    enable = true;
-    theme.name = "Adwaita-dark";
-    
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;  # force dark in all GTK3 apps
-    };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;  # force dark in all GTK4 apps
+    settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";   # Libadwaita
+        gtk-theme = "Adwaita-dark";     # GTK3
+      };
     };
   };
 }
