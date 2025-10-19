ed1a20f7c69130613d266e50d192aae60695f339
Confused-Engineer <dpierce33100@gmail.com>
2025-10-19 17:27:26 -0400

    fix lockscreen monitors

--- machines/desktop/configuration.nix
+++ machines/desktop/configuration.nix
@@ -265,15 +265,15 @@
 
 
 
-  #systemd.services.copyGdmMonitorsXml = {
-  #  description = "Copy monitors.xml to GDM config";
-  #  after = [ "network.target" "systemd-user-sessions.service" "display-manager.service" ];
-  #  serviceConfig = {
-  #    ExecStart = "${pkgs.bash}/bin/bash -c 'echo \"Running copyGdmMonitorsXml service\" && mkdir -p /run/gdm/.config && echo \"Created /run/gdm/.config directory\" && [ \"/home/david/.config/monitors.xml\" -ef \"/run/gdm/.config/monitors.xml\" ] || cp /home/david/.config/monitors.xml /run/gdm/.config/monitors.xml && echo \"Copied monitors.xml to /run/gdm/.config/monitors.xml\" && chown gdm:gdm /run/gdm/.config/monitors.xml && echo \"Changed ownership of monitors.xml to gdm\"'";
-  #    Type = "oneshot";
-  #  };
-  #  wantedBy = [ "multi-user.target" ];
-  #};
+  systemd.services.copyGdmMonitorsXml = {
+    description = "Copy monitors.xml to GDM config";
+    after = [ "network.target" "systemd-user-sessions.service" "display-manager.service" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.bash}/bin/bash -c 'echo \"Running copyGdmMonitorsXml service\" && mkdir -p /run/gdm/.config && echo \"Created /run/gdm/.config directory\" && [ \"/home/david/.config/monitors.xml\" -ef \"/run/gdm/.config/monitors.xml\" ] || cp /home/david/.config/monitors.xml /run/gdm/.config/monitors.xml && echo \"Copied monitors.xml to /run/gdm/.config/monitors.xml\" && chown gdm:gdm /run/gdm/.config/monitors.xml && echo \"Changed ownership of monitors.xml to gdm\"'";
+      Type = "oneshot";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
 
 
 
