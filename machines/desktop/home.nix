dc8ca9cc0af505bf42ab8bb453693ba70c25489d
Confused-Engineer <dpierce33100@gmail.com>
2025-12-09 09:50:48 -0500

    update shell

--- machines/desktop/home.nix
+++ machines/desktop/home.nix
@@ -7,9 +7,10 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      nrs = "sudo nixos-rebuild switch --flake /etc/nixos";
-      ncs = "sudo nix-collect-garbage; sudo nix-collect-garbage -d; sudo nixos-rebuild switch --flake /etc/nixos";
-      nix-upgrade = "sudo nix flake update --flake /etc/nixos && sudo nixos-rebuild boot --flake /etc/nixos";
+      nix-switch = "sudo nixos-rebuild switch --flake /etc/nixos";
+      nix-boot = "sudo nixos-rebuild boot --flake /etc/nixos";
+      nix-test = "sudo nixos-rebuild build-vm --flake /etc/nixos";
+      nix-upgrade = "sudo nix flake update --flake /etc/nixos && sudo nixos-rebuild build-vm --flake /etc/nixos";
       nix-clean = "sudo nix-collect-garbage; sudo nix-collect-garbage -d; sudo nixos-rebuild boot --flake /etc/nixos";
     };
     initExtra = ''
