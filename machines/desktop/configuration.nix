24ec51c1c2c3ff032479193164cbc4f082a72430
Confused-Engineer <dpierce33100@gmail.com>
2025-12-23 11:46:26 -0500

    kde connect desktop

--- machines/desktop/configuration.nix
+++ machines/desktop/configuration.nix
@@ -193,6 +193,7 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  programs.kdeconnect.enable = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
