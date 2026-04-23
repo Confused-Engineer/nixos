fdad9b6e16cdcfb722fb594f459b525f8f90e365
Confused-Engineer <dpierce33100@gmail.com>
2026-04-22 20:57:01 -0400

    maybe

--- flake.nix
+++ flake.nix
@@ -1,31 +1,64 @@
-let
-  mkSystem = { hostname, system ? "x86_64-linux", stateNixpkgs ? nixpkgs-unstable, useHomeManager ? true }:
-    stateNixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = { inherit inputs; };
-      modules = [
-        { nixpkgs.overlays = [ (final: prev: {
-            stable = import nixpkgs { inherit system; config.allowUnfree = true; };
-            unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
-          }) ]; }
-        ./machines/${hostname}/configuration.nix
-      ] ++ lib.optionals useHomeManager [
-        home-manager.nixosModules.home-manager
-        { home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.david = import ./homeManager/david.nix;
-            backupFileExtension = "backup";
-          };
-        }
-      ];
+{
+  description = "Multi-machine NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
-in {
-  nixosConfigurations = {
-    desktop = mkSystem { hostname = "desktop"; };
-    laptop = mkSystem { hostname = "laptop"; };
-    lat9430 = mkSystem { hostname = "lat9430"; stateNixpkgs = nixpkgs; };
-    vacation = mkSystem { hostname = "vacation"; stateNixpkgs = nixpkgs; useHomeManager = false; };
-    kodi = mkSystem { hostname = "kodi"; stateNixpkgs = nixpkgs; useHomeManager = false; };
   };
+
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+    let
+      lib = nixpkgs-unstable.lib;
+
+      mkSystem =
+        { hostname
+        , system ? "x86_64-linux"
+        , stateNixpkgs ? nixpkgs-unstable
+        , useHomeManager ? true
+        }:
+        stateNixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            {
+              nixpkgs.overlays = [
+                (final: prev: {
+                  stable = import nixpkgs {
+                    inherit system;
+                    config.allowUnfree = true;
+                    config.permittedInsecurePackages = [ "mbedtls-2.28.10" ];
+                  };
+                  unstable = import nixpkgs-unstable {
+                    inherit system;
+                    config.allowUnfree = true;
+                  };
+                })
+              ];
+            }
+            ./machines/${hostname}/configuration.nix
+          ] ++ lib.optionals useHomeManager [
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.david = import ./homeManager/david.nix;
+                backupFileExtension = "backup";
+              };
+            }
+          ];
+        };
+    in {
+      nixosConfigurations = {
+        desktop  = mkSystem { hostname = "desktop"; };
+        laptop   = mkSystem { hostname = "laptop"; };
+        lat9430  = mkSystem { hostname = "lat9430"; stateNixpkgs = nixpkgs; };
+        vacation = mkSystem { hostname = "vacation"; stateNixpkgs = nixpkgs; useHomeManager = false; };
+        kodi     = mkSystem { hostname = "kodi";     stateNixpkgs = nixpkgs; useHomeManager = false; };
+      };
+    };
 }
