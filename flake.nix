cc0774edddee64c9eb4aad403e1bcd7c141d0e4b
Confused-Engineer <dpierce33100@gmail.com>
2026-06-12 12:56:14 -0400

    update stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Multi-machine NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
